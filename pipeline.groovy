07f86e7c9d1dc3d3489bab24b259373ad4972f36
pemmasani1200 <74066505+pemmasani1200@users.noreply.github.com>
2020-12-06 12:59:56 -0600

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -3,12 +3,12 @@ pipeline {
     environment {
         branch_name = "master"
         git_url = "https://github.com/pemmasani1200/DevOpsClassCodes.git"
-        SERVICE_NAME = "tomcat-webserver-service"
+        SERVICE_NAME = "ecs-fargate-cluster-svc"
         IMAGE_VERSION = "${BUILD_NUMBER}"
         TASK_FAMILY = "first-run-task-definition"
         DESIRED_COUNT = "1"
         REGION = "us-east-2"
-        CLUSTER_NAME = "NOV16"
+        CLUSTER_NAME = "ecs-fargate-cluster-test"
 		ECR_REPO = "011194234014.dkr.ecr.us-east-2.amazonaws.com/irving"
     }
 
